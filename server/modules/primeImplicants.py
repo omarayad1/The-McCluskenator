9ad44fd532962cd1ba4338bc418eee0a03edcb41
ahmadyasserk <ahmad.yasser@aucegypt.edu>
2014-11-07 15:17:26 +0200

    marked PIs

--- server/modules/primeImplicants.py
+++ server/modules/primeImplicants.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 def combine(m, n):
     a = len(m)
     c = ''
@@ -53,3 +55,51 @@ def get_prime_implicants(data):
         return IM
     else:
         return IM + get_prime_implicants(im2)
+
+
+def perm(inp):
+	f = 0
+	str1=''
+	str2=''
+	for i in xrange(len(inp)):
+		cur_char=inp[i]
+		if (cur_char == "-" and f == 0):
+			str1 += "1"
+			# f = 1
+		else:
+			str1+= cur_char
+
+	f = 0
+	for i in xrange(len(inp)):
+		cur_char=inp[i]
+		if (cur_char == "-" and f == 0):
+			str2 += "0"
+			# f = 1
+		else:
+			str2+= cur_char
+	if (inp == str2 and inp == str1):
+		return 'NONE'
+	else:
+		return [str1,str2]
+
+
+
+def PI(data):
+	liist = list(get_prime_implicants(data))
+	newlist1= [0]*len(liist)
+	newlist0= [0]*len(liist)
+	finallist = []
+
+	for i in xrange(len(liist)):
+		# while !('-' in a):
+		a = perm(liist[i])
+		newlist1[i]=int(a[0], 2)
+		newlist0[i]=int(a[1], 2)
+
+	for i in xrange(len(liist)):
+		bb = [liist[i],newlist0[i],newlist1[i]]
+		finallist.append(bb)
+
+	print finallist
+
+
