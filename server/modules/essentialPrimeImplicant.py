807ef62a8604114c70e260c23c3cd5fe9565f36e
omarayad1 <omarayad1@gmail.com>
2014-11-08 13:21:53 +0200

    added a function to identify the essential prime implicants

---
+++ server/modules/essentialPrimeImplicant.py
@@ -0,0 +1,8 @@
+def get_essential(implicant_dict, terms_list):
+	all_origins = [x for sublist in implicant_dict.values() for x in sublist]
+	essential_implicants = []
+	for term, origins in implicant_dict.iteritems():
+		for origin in origins:
+			if all_origins.count(origin) == 1 and origin in terms_list:
+				essential_implicants.append(term)
+	return essential_implicants
